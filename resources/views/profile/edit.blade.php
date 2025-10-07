c4a950de2fae37ed00cfa5c012385456bf2718f6
N-VenZ13 <staycool006@gmail.com>
2025-10-07 14:38:13 +0700

    tambah tombol button

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,6 +13,8 @@
                 </div>
             </div>
 
+
+
             <!-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
@@ -24,6 +26,26 @@
                     @include('profile.partials.delete-user-form')
                 </div>
             </div> -->
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <h2 class="text-lg font-medium text-gray-900">
+                        Log Out
+                    </h2>
+                    <p class="mt-1 text-sm text-gray-600">
+                        Keluar dari sesi Anda saat ini.
+                    </p>
+                    <form method="POST" action="{{ route('logout') }}" class="mt-6">
+                        @csrf
+                        <button type="submit"
+                            class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            Log Out
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </x-main-layout>
